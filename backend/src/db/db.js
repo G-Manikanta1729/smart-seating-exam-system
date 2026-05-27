12a4e5e673396a5306475c84c107836d16eceee0
Grandhe Manikanta Sri Sai Kiran <grandhemanikanta65@gmail.com>
2026-05-27 23:53:27 +0530

    Change database connection string environment variable

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,7 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const db = new Pool({
-  connectionString: process.env.MYSQL_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
